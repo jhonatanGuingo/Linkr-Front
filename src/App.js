7fe6473d432a3f378aa0d391ba51107ceee13cc5
MilenaMed <115955806+MilenaMed@users.noreply.github.com>
2023-08-15 21:25:34 -0300

    Organização inicial e configurações basicas

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import App from './App';
-
+import App from './App.js';
+import ResetStyle from './styled/resetStyled.js';
+import GlobalStyle from './styled/globalStyled.js';
 
 ReactDOM.render(
   <React.StrictMode>
+    <ResetStyle />
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
