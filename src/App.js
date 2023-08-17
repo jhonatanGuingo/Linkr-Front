71d5363a317b2241e0b50f3bf36914ee188e890f
DanielCamposDevX <daniel.campos.dev3@gmail.com>
2023-08-16 22:13:18 -0300

    Base

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Likes from "./components/likes";
+
+
+export default function App(){
+
+    return(
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+            <Likes id='1'/>
+        </div>
+    )
+}
